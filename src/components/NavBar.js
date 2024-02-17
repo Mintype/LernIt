73ff57dbcc139a318a67fec0f99734933a2ee495
SmallPlayz <900raiyan@gmail.com>
2024-02-17 16:54:16 -0500

    fixxed navbar routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -125,7 +125,7 @@ function NavBar() {
     <div>
         <ul className="navbar">
             <li className="title" id="nav-item"><a href="/">LernIt</a></li>
-            <li className="nav-item" id="nav-item"><a href="/about">My Sets</a></li>
+            <li className="nav-item" id="nav-item"><a href="/mysets">My Sets</a></li>
             <li className="nav-item" id="nav-item"><a href="/new">New</a></li>
 
 
@@ -186,7 +186,7 @@ function NavBar() {
           */}
 
             <li className="nav-item" id="nav-item"><a href="/about">About</a></li>
-            <li className="nav-item" id="nav-item"><a href="/new">Me</a></li>
+            <li className="nav-item" id="nav-item"><a href="/me">Me</a></li>
         </ul>
     </div>
   )
