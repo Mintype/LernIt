dcbf66f804bb1d79567e88b9f5b9c6bab5b994f6
SmallPlayz <900raiyan@gmail.com>
2024-02-16 19:21:08 -0500

    search bar upgraded!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,81 @@
 import React from 'react'
 import '../styles/navbar.css';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 function NavBar() {
+
+    const items = [
+        {
+          id: 0,
+          name: 'Cobol'
+        },
+        {
+          id: 1,
+          name: 'JavaScript'
+        },
+        {
+          id: 2,
+          name: 'Basic'
+        },
+        {
+          id: 3,
+          name: 'PHP'
+        },
+        {
+          id: 4,
+          name: 'Java'
+        }
+      ]
+    
+      const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        console.log(string, results)
+      }
+    
+      const handleOnHover = (result) => {
+        // the item hovered
+        console.log(result)
+      }
+    
+      const handleOnSelect = (item) => {
+        // the item selected
+        console.log(item)
+      }
+    
+      const handleOnFocus = () => {
+        console.log('Focused')
+      }
+    
+      const formatResult = (item) => {
+        return (
+            <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+        )
+      }
+
   return (
     <div>
         <ul class="navbar">
             <li class="title" id="nav-item"><a href="/">LernIt</a></li>
+            <li class="nav-item" id="nav-item"><a href="/about">My Sets</a></li>
             <li class="nav-item" id="nav-item"><a href="/new">New</a></li>
-            <li class="nav-item" id="nav-item"><a href="/contact">Contact</a></li>
+            <ReactSearchAutocomplete
+            className="nav-search"
+            placeholder="Search.."
+            inputDebounce="200"
+            items={items}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+            styling={{
+                height: "35px"
+            }}
+            />
             <li class="nav-item" id="nav-item"><a href="/about">About</a></li>
-            <li class="nav-item" id="nav-item"><a href="/">Home</a></li>
+            <li class="nav-item" id="nav-item"><a href="/new">Me</a></li>
         </ul>
     </div>
   )
