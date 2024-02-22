9d04858a1dfe916210d4d2880611a9df12d09ffd
SmallPlayz <900raiyan@gmail.com>
2024-02-22 15:59:47 -0500

    Fixxed UserInfo auth problems.

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { auth } from "../firebase-config";
 
 function UserInfo() {
+  const [displayName, setDisplayName] = useState('Unknown User');
+  const [photoURL, setPhotoURL] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const name = user.displayName;
+        const photoURL = user.photoURL; // Get the user's profile picture URL
+        if (name) {
+          setDisplayName(name);
+        }
+        if (photoURL) {
+          setPhotoURL(photoURL);
+        }
+      } else {
+        setDisplayName('Unknown User');
+        setPhotoURL(null);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>UserInfo</div>
-  )
+    <div>
+      <h1>User Information</h1>
+      <h1>{displayName}</h1>
+      {photoURL && <img src={photoURL} alt="Profile" />}
+    </div>
+  );
 }
 
-export default UserInfo
+export default UserInfo;
